c83a72016a9e3caaef8e759c79d6124509f90723
StevenMadera <86321979+StevenMadera@users.noreply.github.com>
2021-08-31 19:57:23 -0500

    Login y Crud vinculado

---
+++ Al_Shop/Controllers/UsuarioController.cs
@@ -0,0 +1,34 @@
+﻿using Al_Shop.Models;
+using Al_Shop.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Al_Shop.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            List<ListUsuarioViewModel> lista;
+
+            using (Al_shopEntities bd = new Al_shopEntities())
+            {
+                lista = (from u in bd.Usuarios
+                         select new ListUsuarioViewModel
+                         {
+                             Id = u.id,
+                             Nombre = u.nombre,
+                             Email = u.email,
+                             Telefono = u.telefono,
+                             Fechan = u.fechan,
+                             Pass = u.pass
+                         }).ToList();
+            }
+                return View(lista);
+        }
+    }
+}
---
+++ Al_Shop/Models/ViewModels/ListUsuarioViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Al_Shop.Models.ViewModels
+{
+    public class ListUsuarioViewModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public long Telefono { get; set; }
+        public System.DateTime Fechan { get; set; }
+        public byte[] Pass { get; set; }
+    }
+}
